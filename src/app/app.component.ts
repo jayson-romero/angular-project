58ae6864178be23cd87b1275bd4af4f6993cc6f0
Jayson Romero <jaysonromero1218@gmail.com>
2024-09-18 17:02:23 +0800

    adding other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CatalogComponent, SiteHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'iton-robot-shop';
-}
+export class AppComponent {}
---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-header',
+  standalone: true,
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css'],
+})
+export class SiteHeaderComponent {
+  constructor() {}
+}
